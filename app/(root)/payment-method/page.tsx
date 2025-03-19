54d0461042aa02ab756bf21df2237ce0c5f1af6e
cepoumian <cepoumian@gmail.com>
2025-03-18 21:32:28 -0600

    Add payment method page

---
+++ app/(root)/payment-method/page.tsx
@@ -0,0 +1,31 @@
+import { Metadata } from "next";
+import { auth } from "@/auth";
+import { getUserById } from "@/lib/actions/user.actions";
+import PaymentMethodForm from "./payment-method-form";
+
+export const metadata: Metadata = {
+  title: "Select Payment Method",
+};
+
+const PaymentMethodPage = async () => {
+  const session = await auth();
+  const userId = session?.user?.id as string;
+
+  if (!userId) {
+    throw new Error("User not found");
+  }
+
+  const user = await getUserById(userId);
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  return (
+    <>
+      <PaymentMethodForm preferredPaymentMethod={user.paymentMethod} />
+    </>
+  );
+};
+
+export default PaymentMethodPage;
---
+++ app/(root)/payment-method/payment-method-form.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/hooks/use-toast";
+import { useTransition } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { paymentMethodSchema } from "@/lib/validators";
+import CheckoutSteps from "@/components/shared/checkout-steps";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { DEFAULT_PAYMENT_METHOD } from "@/lib/constants";
+
+const PaymentMethodForm = ({
+  preferredPaymentMethod,
+}: {
+  preferredPaymentMethod: string | null;
+}) => {
+  const router = useRouter();
+  const { toast } = useToast();
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof paymentMethodSchema>>({
+    resolver: zodResolver(paymentMethodSchema),
+    defaultValues: { type: preferredPaymentMethod || DEFAULT_PAYMENT_METHOD },
+  });
+
+  return (
+    <>
+      <CheckoutSteps current={2} />
+    </>
+  );
+};
+
+export default PaymentMethodForm;
